57e42082f827a8d044c175f89ed479d9973ce5d5
“licheng-iwish” <522974878@qq.com>
2023-10-18 21:24:25 +0800

    去除无用输入

--- demo/serving/ocr_det_preprocess/1/model.py
+++ demo/serving/ocr_det_preprocess/1/model.py
@@ -92,11 +92,8 @@ class TritonPythonModel:
             boxes = pb_utils.get_input_tensor_by_name(request,
                                                      self.input_names[1])
             boxes = boxes.as_numpy()
-            labels = pb_utils.get_input_tensor_by_name(request,
-                                                     self.input_names[2])
-            labels = labels.as_numpy()
             origin0 = pb_utils.get_input_tensor_by_name(request,
-                                                     self.input_names[3])
+                                                     self.input_names[2])
             origin0 = origin0.as_numpy()
             data = []
             for i in range(origin0.shape[0]):
